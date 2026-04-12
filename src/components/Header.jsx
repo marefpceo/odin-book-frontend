4e23e1bb7eeaaab3089e978951b32d68429cbce1
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-04-12 16:47:50 -0700

    Replaced material icons with font awesome icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,37 @@
 import { Link } from "react-router";
 import logoNoName from "../assets/odin-book-name-no-logo-350w.png";
-import homeIcon from "../assets/homeIcon.svg";
-import profileIcon from "../assets/profileIcon.svg";
-import usersIcon from "../assets/usersIcon.svg";
-import menuIcon from "../assets/menuIcon.svg";
-import newPostIcon from "../assets/newPostIcon.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faSquarePlus,
+  faCircleUser,
+} from "@fortawesome/free-regular-svg-icons";
+import { faBars, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
     <header
-      className="pt-1 dark:border-b-odinbook-altDark/25 dark:border-b 
-      dark:shadow-[0_-5px_10px] dark:shadow-odinbook-light"
+      className="pt-1 dark:border-b-odinbook-altDark/25 dark:border-b dark:shadow-[0_-5px_10px] 
+      dark:shadow-odinbook-light"
     >
-      <div className="mx-2 flex justify-between">
-        <div className="flex gap-x-2">
-          <img src={menuIcon} alt="Menu icon" width={28} />
+      <div className="mx-2 flex justify-between items-center">
+        <div className="flex gap-x-2 items-center">
+          <FontAwesomeIcon icon={faBars} size="lg" />
           <img src={logoNoName} alt="Odin Book name without logo" width={175} />
         </div>
         <Link to={"#newPost"}>
-          <img src={newPostIcon} alt="New post icon" width={28} />
+          <FontAwesomeIcon icon={faSquarePlus} size="lg" />
         </Link>
       </div>
-      <div className="mt-4 pb-1 flex justify-evenly text-odinbook-altDark border-b border-odinbook-altDark/45">
+      <div className="mt-4 pb-1 flex justify-evenly border-b border-odinbook-altDark/45">
         <Link to={"/"}>
-          <img src={homeIcon} alt="Home icon" width={30} />
+          <FontAwesomeIcon icon={faHouse} size="lg" />
         </Link>
         <Link to={"#users"}>
-          <img src={usersIcon} alt="Users icon" width={30} />
+          <FontAwesomeIcon icon={faUserGroup} size="lg" />
         </Link>
         <Link to={"#profile"}>
-          <img src={profileIcon} alt="Profile icon" width={30} />
+          <FontAwesomeIcon icon={faCircleUser} size="lg" />
         </Link>
       </div>
     </header>
