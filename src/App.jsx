481142565cae2790ce5752cb38744107b3d88498
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-04-11 23:05:46 -0700

    Added fonts to base theme

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router'
 function App() {
 
   return (
-    <div className='bg-odinbook-denim'>
+    <div className='bg-odinbook-denim font-logo'>
       <section id="center">
-        <h1>Odin Book - Layout</h1>
+        <h1 className='font-body'>Odin Book - Layout</h1>
         <Outlet />
       </section>
     </div>
