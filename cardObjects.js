c295485f47350055ad6a4edf737aa0b935bfc2ec
marefpceo <marefpceo@gmail.com>
2026-04-12 20:13:41 -0700

    Created cardObjects helper to test Card layout

---
+++ cardObjects.js
@@ -0,0 +1,52 @@
+const cards = [
+  {
+    id: 1,
+    author: "Postman",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis!",
+    likes: 4,
+    comments: 3,
+  },
+  {
+    id: 2,
+    author: "Postman",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis!",
+    likes: 4,
+    comments: 3,
+  },
+  {
+    id: 3,
+    author: "Postman",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis!",
+    likes: 4,
+    comments: 3,
+  },
+  {
+    id: 4,
+    author: "Postman",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis!",
+    likes: 4,
+    comments: 3,
+  },
+  {
+    id: 5,
+    author: "Postman",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis!",
+    likes: 4,
+    comments: 3,
+  },
+  {
+    id: 6,
+    author: "Postman",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis!",
+    likes: 4,
+    comments: 3,
+  },
+];
+
+export default cards;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
 import Card from "../components/Card";
 
+import cards from "../../cardObjects"; // Remove after
+
 function Home() {
   return (
     <section role="main" className="home">
-      <Card />
+      {cards.map((card) => (
+        <Card
+          postAuthor={card.author}
+          postContent={card.content}
+          likes={card.likes}
+          commentCount={card.comments}
+        />
+      ))}
     </section>
   );
 }
