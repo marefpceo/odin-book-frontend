0d8a32464fa80c8541a1a1ad23fe17dfc4edf8fc
marefpceo <32721274+marefpceo@users.noreply.github.com>
2026-04-12 20:51:22 -0700

    Added link to profile and updated styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,26 @@
+import { Link } from "react-router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
 import { faThumbsUp, faComment } from "@fortawesome/free-regular-svg-icons";
 
 function Card({ postAuthor, postContent, likes, commentCount }) {
   return (
-    <div className="p-2 w-full border-b border-odinbook-dark rounded-xs">
+    <div className="p-2 w-full border-b border-odinbook-dark rounded-xs dark:border-darkmode-altDark">
       <div>
-        <FontAwesomeIcon icon={faCircleUser} size="2xl" />
-        <p>{postAuthor}</p>
+        <Link to={"#selectedProfile"} className="flex gap-x-3 items-center">
+          <FontAwesomeIcon icon={faCircleUser} size="2xl" />
+          <p className="font-bold">{postAuthor}</p>
+        </Link>
       </div>
-      <p>{postContent}</p>
-      <div className="flex items-center">
+      <div className="py-4">
+        <p>{postContent}</p>
+      </div>
+      <div className="flex items-center gap-x-4">
         <span className="flex">
           <FontAwesomeIcon icon={faThumbsUp} size="lg" />
           <p>{likes}</p>
         </span>
-        <span>
+        <span className="flex">
           <FontAwesomeIcon icon={faComment} size="lg" />
           <p>{commentCount}</p>
         </span>
